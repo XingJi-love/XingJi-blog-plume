efd2395d5562fdaced949c95b78a5ddc31480103
XingJi-love <3968523679@qq.com>
2025-07-26 18:43:59 +0800

    构造方法完结撒花

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -46,7 +46,7 @@ export default defineThemeConfig({
   ],
   navbarSocialInclude: ['github','gitee','csdn','leetcode','bilibili'], // 允许显示在导航栏的 social 社交链接
   // aside: true, // 页内侧边栏， 默认显示在右侧
-  // outline: [2, 3], // 页内大纲， 默认显示 h2, h3
+  outline: [1, 6], // 页内大纲， 默认显示h1-h6标题 
 
   /**
    * 文章版权信息
